57a41bfd63fe14b521fd73e3f59a20b1c072978b
kirienkovitaliy <kirienkovitaliy1989@gmail.com>
2023-04-25 19:52:37 +0300

    Update factorize.py

--- factorize.py
+++ factorize.py
@@ -1,11 +1,38 @@
-def factorize(*number):
-    # YOUR CODE HERE
-    raise NotImplementedError() # Remove after implementation
+import time
+from multiprocessing import Pool, cpu_count
 
 
-a, b, c, d  = factorize(128, 255, 99999, 10651060)
+def factorize(numbers):
+    my_list = []
+    for number in numbers:
+        div = [x for x in range(1, number // 2 + 1) if number % x == 0]
+        div.append(number)
+        my_list.append(div)
+    return my_list
 
-assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+
+def factorize_synchr(numbers):
+    for number in numbers:
+        div = [x for x in range(1, number // 2 + 1) if number % x == 0]
+        div.append(number)
+    return div
+
+
+def factorize_multi(numbers):
+    with Pool(cpu_count()) as pool:
+        return pool.map(factorize_synchr, numbers)
+
+
+numbers = [128, 255, 99999, 10651060]
+
+start_time = time.time()
+process = factorize(numbers)
+end_time = time.time()
+result = end_time - start_time
+print(f"Synchronous CPU time: {result}")
+
+start_time = time.time()
+process = factorize_multi(numbers)
+end_time = time.time()
+result = end_time - start_time
+print(f"Multiprocessing time: {result}")
